0447cd44a8257e3115b329d83b2854b2e51febfb
rafaseara <rafaseara257@gmail.com>
2025-03-12 18:41:58 -0300

    Colocando um tratamento da excecao da def escolher_opcao

--- app.py
+++ app.py
@@ -20,22 +20,32 @@ def finalizar_app():
     os.system('cls')
     print('Encerrando o programa\n')
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    if opcao_escolhida == 1:
-        print('Você selecionou a opção de Cadastrar')
-    elif opcao_escolhida == 2:
-        print('Você selecionou a opção de Listar')
-    elif opcao_escolhida == 3:
-        print('Você selecionou a opção de Ativar')
-    else:
-        finalizar_app()
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        if opcao_escolhida == 1:
+            print('Você selecionou a opção de Cadastrar')
+        elif opcao_escolhida == 2:
+            print('Você selecionou a opção de Listar')
+        elif opcao_escolhida == 3:
+            print('Você selecionou a opção de Ativar')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
 
 if __name__ == '__main__':
     main()
-
