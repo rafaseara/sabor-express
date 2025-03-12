4a98192dce63bd6e8a3a39e09418b8cc67d0a9a8
rafaseara <rafaseara257@gmail.com>
2025-03-12 16:00:13 -0300

    Iniciando o programa

---
+++ app.py
@@ -0,0 +1,9 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+# shift + alt copia linhas
+# ctrl + B fecha a barra lateral
