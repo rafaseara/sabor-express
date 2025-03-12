a8f637770b0524a5d4366ca12a4968ea188a3431
rafaseara <rafaseara257@gmail.com>
2025-03-12 19:33:42 -0300

    Refatorando o código para evitar redundâncias

--- app.py
+++ app.py
@@ -19,32 +19,38 @@ def exibir_opcoes():
     # ctrl + B fecha a barra lateral
 
 def finalizar_app():
-    os.system('cls')
-    print('Encerrando o programa\n')
+    exibir_subtitulo('Finalizar app')
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()   
+    print()
+
+def exibir_subtitulo(texto):
+     os.system('cls')
+     print(texto)
 
 def opcao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurante')
+    exibir_subtitulo('Cadastro de novos restaurante')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante) #colocar algo na lista
     print(f'O {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 def listar_restaurantes():
-    os.system('cls')
-    print('Listagem dos restaurantes')
+    exibir_subtitulo('Listagem dos restaurantes')
 
     for indice, restaurante in enumerate(restaurantes, start=1):
         print(f'{indice} - {restaurante}')
+    #indice vai corresponder ao numero e pode ser qualquer nome
+    #restaurante vai ser o nome da variavel
+    #restaurantes corresponde a lista
 
-    input('\nDigite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 def escolher_opcao():
     try:
