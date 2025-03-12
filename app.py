01a3297661f6859fcbe782a217ef58e8a1a383e0
rafaseara <rafaseara257@gmail.com>
2025-03-12 18:09:19 -0300

    Organizando o código criando funções e definindo o main

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-print('Sabor Express\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair')
+def exibir_nome_do_programa():
+    print(""" 
+    ██████████████████████████████████████████████████████████████████████████
+    █─▄▄▄▄██▀▄─██▄─▄─▀█─▄▄─█▄─▄▄▀███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█
+    █▄▄▄▄─██─▀─███─▄─▀█─██─██─▄─▄████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█
+    ▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▀▀▄▄▄▄▀▄▄▀▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀  
+        """)
 
-# shift + alt copia linhas
-# ctrl + B fecha a barra lateral
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+    # shift + alt copia linhas
+    # ctrl + B fecha a barra lateral
+
+def finalizar_app():
+    os.system('cls')
+    print('Encerrando o programa\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    if opcao_escolhida == 1:
+        print('Você selecionou a opção de Cadastrar')
+    elif opcao_escolhida == 2:
+        print('Você selecionou a opção de Listar')
+    elif opcao_escolhida == 3:
+        print('Você selecionou a opção de Ativar')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
+
